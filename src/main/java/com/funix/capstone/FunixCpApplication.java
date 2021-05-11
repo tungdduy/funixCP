8926e158931e039123bdb7465be7f5c75650d142
tungdduy <tunG@1993>
2021-05-11 10:47:34 +0700

    add log4j2

--- src/main/java/com/funix/capstone/FunixCpApplication.java
+++ src/main/java/com/funix/capstone/FunixCpApplication.java
@@ -1,9 +1,12 @@
 package com.funix.capstone;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class FunixCpApplication {
 
     public static void main(String[] args) {
