8424a758174fe859901b6abc6204678da8e2b61a
tungdduy <tunG@1993>
2021-05-11 10:45:21 +0700

    Initial commit

---
+++ src/main/java/com/funix/capstone/FunixCpApplication.java
@@ -0,0 +1,13 @@
+package com.funix.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunixCpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunixCpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/funix/capstone/FunixCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funix.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunixCpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
