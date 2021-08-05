74d050dc9c08be5cc8caa0a4c9c4f920c1ae9d3f
giapsoft <giapsoft@gmail.com>
2021-08-05 22:15:15 +0700

    REGISTER, EDIT ACCOUNT INFO, FIX SOME DATA TABLES AS PREVIOUS DISCUSSION.

--- SourceCode/XeKhachSpring/src/main/java/net/timxekhach/operation/data/entity/User.java
+++ SourceCode/XeKhachSpring/src/main/java/net/timxekhach/operation/data/entity/User.java
@@ -4,23 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import net.timxekhach.operation.data.mapped.User_MAPPED;
-import net.timxekhach.operation.data.repository.UserRepository;
 import net.timxekhach.operation.response.ErrorCode;
 import net.timxekhach.security.handler.SecurityConfig;
-import net.timxekhach.security.model.SecurityResource;
 import net.timxekhach.utility.XeStringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.web.multipart.MultipartFile;
-import javax.persistence.*;
-import java.io.File;
-import java.io.IOException;
+
+import javax.persistence.Entity;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import static net.timxekhach.operation.response.ErrorCode.EMAIL_EXISTED;
+
 import static net.timxekhach.utility.XeMailUtils.sendEmailRegisterSuccessFully;
+
 // ____________________ ::IMPORT_SEPARATOR:: ____________________ //
 
 @Entity @Getter @Setter
